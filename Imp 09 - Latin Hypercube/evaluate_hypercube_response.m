af61519b78e7b5048d57bdfedb5d0314c0c8de82
Fernando Laube <fernando.laube@gmail.com>
2020-12-21 13:51:12 -0300

    Update convergence logic in 'evaluate_hypercube_response'

--- Imp 09 - Latin Hypercube/evaluate_hypercube_response.m
+++ Imp 09 - Latin Hypercube/evaluate_hypercube_response.m
@@ -1,8 +1,11 @@
 function [energy, convergence, smoothness] = evaluate_hypercube_response(V, T, x, y, z, x_d, y_d, z_d, t_array)
 
     energy = trapz(t_array(2:end), T.*V(2:end));
-    distance_to_required = sum(abs(x - x_d)./x_d + abs(y - y_d)./y_d + abs(z - z_d)./z_d);
+    distance_to_required = zeros(1, length(x)*3);
+    distance_to_required(1:length(x)) = abs(x - x_d)./x_d;
+    distance_to_required(1+length(x):2*length(x)) = abs(y - y_d)./y_d;
+    distance_to_required(1+2*length(x):3*length(x)) = abs(z - z_d)./z_d;
     
-    convergence = (distance_to_required <= (0.05*length(x)));
+    convergence = all(distance_to_required <= 0.05);
     
     smoothness = 0;
