2bbf9808e066afab87c61a4d9cffc9179f287a88
Fernando Laube <fernando.laube@gmail.com>
2021-01-06 13:40:33 -0300

    Modifies convergence calculation

--- Imp 10 - Parameters Optimization/evaluate_hypercube_response.m
+++ Imp 10 - Parameters Optimization/evaluate_hypercube_response.m
@@ -1,14 +1,16 @@
 function [energy, convergence, smoothness] = evaluate_hypercube_response(V, T, x, y, z, x_d, y_d, z_d, past_x, past_y, past_z, t_array)
 
     energy = trapz(t_array(2:end), T.*V(2:end));
-    distance_to_required = zeros(1, length(x)*3);
-    distance_to_required(1:length(x)) = abs(x - x_d)./x_d;
-    distance_to_required(1+length(x):2*length(x)) = abs(y - y_d)./y_d;
-    distance_to_required(1+2*length(x):3*length(x)) = abs(z - z_d)./z_d;
+    % distance_to_required = zeros(1, length(x)*3);
+    % distance_to_required(1:length(x)) = abs(x - x_d)./x_d;
+    % distance_to_required(1+length(x):2*length(x)) = abs(y - y_d)./y_d;
+    % distance_to_required(1+2*length(x):3*length(x)) = abs(z - z_d)./z_d;
     
     % convergence = all(distance_to_required <= 0.05);
     
-    convergence = sum(distance_to_required);
+    % convergence = sum(distance_to_required);
+    
+    convergence = sum((x_d - x).^2 + (y_d - y).^2 + (z_d - z).^2);
     
     diff_x = diff(past_x);
     diff_y = diff(past_y);
