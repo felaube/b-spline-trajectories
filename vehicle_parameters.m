46cddc3bcea5a7c9618eaa93c5b1a15cc32a4542
Fernando Laube <fernando.laube@gmail.com>
2021-03-21 16:31:52 -0300

    Reorder the vehicles in 'vehicle_parameters.m'

--- vehicle_parameters.m
+++ vehicle_parameters.m
@@ -7,42 +7,6 @@ function [e, S, b, T_max, mass, AR, k, C_D_0, ...
         aircrafts.
     %}
     switch vehicle_id
-        case 0 % DECODE 2
-            % Oswald factor [-]
-            e = 0.8;
-
-            % Wing area [m²]
-            S = 1.554;
-
-            % Span [m]
-            b = 3.9422;
-
-            % Maximum thrust [N]
-            T_max = 130;
-
-            % Aircraft mass [kg]
-            mass = 25;
-
-            % Aspect Ratio [-]
-            AR = b^2/S;
-
-            % k value [-]
-            k = 0.0334;
-
-            % Cd0 value [-]
-            C_D_0 = 0.0143;
-
-            gamma_max = deg2rad(20);
-            gamma_min = deg2rad(-20);
-
-            phi_max = deg2rad(30);
-            phi_min = deg2rad(-30);
-
-            n_max = 3.8;
-            n_min = -1.5;
-
-            V_min_drag = sqrt(mass*G*2/(RHO*S))*((k/C_D_0)^(1/4));
-            T_min = 1/2*RHO*S*2*C_D_0.*V_min_drag.^2;
 
         case 1 % DECODE 1
             % Wing area [m²]
@@ -80,4 +44,41 @@ function [e, S, b, T_max, mass, AR, k, C_D_0, ...
 
             V_min_drag = sqrt(mass*G*2/(RHO*S))*((k/C_D_0)^(1/4));
             T_min = 1/2*RHO*S*2*C_D_0.*V_min_drag.^2;
+            
+        case 2 % DECODE 2
+            % Oswald factor [-]
+            e = 0.8;
+
+            % Wing area [m²]
+            S = 1.554;
+
+            % Span [m]
+            b = 3.9422;
+
+            % Maximum thrust [N]
+            T_max = 130;
+
+            % Aircraft mass [kg]
+            mass = 25;
+
+            % Aspect Ratio [-]
+            AR = b^2/S;
+
+            % k value [-]
+            k = 0.0334;
+
+            % Cd0 value [-]
+            C_D_0 = 0.0143;
+
+            gamma_max = deg2rad(20);
+            gamma_min = deg2rad(-20);
+
+            phi_max = deg2rad(30);
+            phi_min = deg2rad(-30);
+
+            n_max = 3.8;
+            n_min = -1.5;
+
+            V_min_drag = sqrt(mass*G*2/(RHO*S))*((k/C_D_0)^(1/4));
+            T_min = 1/2*RHO*S*2*C_D_0.*V_min_drag.^2;         
     end
